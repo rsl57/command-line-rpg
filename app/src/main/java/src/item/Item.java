a3ea7dad2f66554836aa7af4a94423e672f02b0c
Ryan Larsen <rsl57@txstate.edu>
2022-03-11 22:07:32 -0600

    Implemented check_quantity, tash, and add_quantity functions to Item class

--- app/src/main/java/src/item/Item.java
+++ app/src/main/java/src/item/Item.java
@@ -15,4 +15,24 @@ public abstract class Item {
     public boolean is_consumable() {
         return CONSUMABLE;
     }
+
+    public int check_quantity() {
+        return quantity;
+    }
+
+    public void trash(int quantity) {
+        if(this.quantity >= quantity) {
+            this.quantity -= quantity;
+        } else {
+            this.quantity = 0;
+        }
+    }
+
+    public void add_quantity(int quantity) {
+        if(this.quantity + quantity <= MAX_STACK) {
+            this.quantity += quantity;
+        } else {
+            this.quantity = MAX_STACK;
+        }
+    }
 }
