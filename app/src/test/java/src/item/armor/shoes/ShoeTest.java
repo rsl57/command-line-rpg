82da3c7c1bfaa88157490942f12dfb504e6cf820
Ryan Larsen <rsl57@txstate.edu>
2022-03-15 10:30:19 -0500

    Removed unused import from shoe test cases

--- app/src/test/java/src/item/armor/shoes/ShoeTest.java
+++ app/src/test/java/src/item/armor/shoes/ShoeTest.java
@@ -1,7 +1,6 @@
 package src.item.armor.shoes;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShoeTest {
